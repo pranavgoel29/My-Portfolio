d9c15e2c6d545a10774cb5737435273f943db12d
pranavgoel29 <pranavgoel29@gmail.com>
2022-10-07 19:19:54 +0530

    Initial commit

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">Pranav Goel
+        <img src="../src/assets/hand.svg" alt="" />
+        </h1>
+
+        <h3 className="home__substitle">Visual Designer</h3>
+        <p className="home__description">
+            I'm Frontend developer based in India, and I'm really passionate and dedicated to my work.
+        </p> 
+
+        <a href="#contact" className="button button--flex">Say Hello
+        <img src="../src/assets/send.svg" alt="" />
+        </a>
+    </div>
+  )
+}
+
+export default Data
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/pranavgoel_29/" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
+
+        <a href="https://twitter.com/Pranavgoel_29" className="home__social-icon" target="_blank"><i className="uil uil-twitter"></i></a>
+
+        <a href="https://github.com/pranavgoel29" className="home__social-icon" target="_blank"><i className="uil uil-github"></i></a>
+    </div>
+  )
+}
+
+export default Social
