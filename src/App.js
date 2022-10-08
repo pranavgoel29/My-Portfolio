402b446b006d2eb951b3fdd27c32ef988ac0ceee
pranavgoel29 <pranavgoel29@gmail.com>
2022-10-08 16:47:48 +0530

    Adding qualification section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
+import Qualification from './components/qualification/Qualification'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Qualification />
     </main>
     </> 
   )
---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import "./qualification.css";
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My Personal Journey</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+            Education
+          </div>
+          <div className="qualification__button button--flex">
+            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div className="qualification__content qualification__content-active">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  B.Tech (Computer Science)
+                </h3>
+                <span className="qualification__subtitle">India - LPU</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Art Director</h3>
+                <span className="qualification__subtitle">India - LPU</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web Developement</h3>
+                <span className="qualification__subtitle">India - LPU</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">UX Expert</h3>
+                <span className="qualification__subtitle">India - LPU</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Frontend Developer
+                </h3>
+                <span className="qualification__subtitle">Yellow - India</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            {/* <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Art Director</h3>
+                <span className="qualification__subtitle">India - LPU</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+            </div> */}
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Open Source</h3>
+                <span className="qualification__subtitle">India</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
