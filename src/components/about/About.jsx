d41321c8bcc4dc0263eeb06a53f0b06e87528c3e
pranavgoel29 <pranavgoel29@gmail.com>
2022-10-08 11:32:31 +0530

    Added CV button with fixed info file initial

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,11 +15,12 @@ function About() {
 
             <div className="about__data">
                 <Info />
+
                 <p className="about__description">
                     Frontend developer, I create web pages with UI/UX user interface, I have years of experience and many clients are happy with the projects carried out.
                 </p>
 
-                <a download="" href={CV} className="button button--flex">Download CV 
+                <a download='' href={CV} className="button button--flex">Download CV 
                 <svg
                 class="button__icon"
                 xmlns="http://www.w3.org/2000/svg"
