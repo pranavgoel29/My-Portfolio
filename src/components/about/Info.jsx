7cee1bf22290324e3859bbf20b4527d60eb7cce0
pranavgoel29 <pranavgoel29@gmail.com>
2022-10-08 11:49:22 +0530

    Corrected padding of the about us section and layout

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Info() {
   return (
-    <div>Info</div> 
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class='bx bx-award about__icon'></i>
+
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">0-1 Years</span>
+      </div>
+
+      <div className="about__box">
+      <i className="bx bx-briefcase-alt about__icon"></i>
+
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">8 + Projects</span>
+      </div>
+
+    </div>
   )
 }
 
