46bc6c238b41b4f148032c335bdad9630169c354
pranavgoel29 <pranavgoel29@gmail.com>
2022-10-08 12:40:49 +0530

    Adding favicon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-import About from './components/about/About'
+import About from './components/about/About';
 
 const App = () => {
   return (
     <>
     <Header />
-
     <main className='main'>
       <Home />
       <About />
